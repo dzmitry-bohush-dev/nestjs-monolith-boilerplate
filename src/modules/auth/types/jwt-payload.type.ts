974b6ca188bebdb86a77b3d60daa3dae5a46e81f
dzmitry-bohush-dev <dzmitry.bohush@innowise.com>
2026-09-08 11:58:42 +0200

    Update JWT types

--- src/modules/auth/types/jwt-payload.type.ts
+++ src/modules/auth/types/jwt-payload.type.ts
@@ -3,6 +3,28 @@ export interface JwtPayload {
   email: string;
 }
 
+export interface AccessTokenPayload {
+  sub: string;
+  email: string;
+  type: 'access';
+}
+
+export interface RefreshTokenPayload {
+  sub: string;
+  type: 'refresh';
+}
+
+interface JwtStandardClaims {
+  iat: number;
+  exp: number;
+  iss?: string;
+  aud?: string;
+}
+
+export type DecodedAccessTokenPayload = AccessTokenPayload & JwtStandardClaims;
+export type DecodedRefreshTokenPayload = RefreshTokenPayload &
+  JwtStandardClaims;
+
 export interface AuthenticatedUser {
   userId: string;
   email: string;
